c8722e9ffb7fb0614d898e09fc37c8c5c6fd2bd6
hzz <huzhangzheng@live.com>
2018-06-26 13:20:48 +0800

    phpunit 约束

--- oop/18/userstoreTest.php
+++ oop/18/userstoreTest.php
@@ -10,13 +10,27 @@ class UserStoreTest extends PHPUnit_Framework_TestCase{
 
     }
     function testGetUser(){
-        $this->store->addUser('bob','a@c.com','1234');
+        $this->store->addUser('bob','a@c.com','123456');
         $user=$this->store->getUser('a@c.com');
         $this->assertEquals($user['mail'],'a@c.com');
         $this->assertEquals($user['name'],'bob');
-        $this->assertEquals($user['pass'],'123');
+        $this->assertEquals($user['pass'],'123456');
     }
     function testMy(){
 
+    }
+    function testAddUser_duplicate(){
+        try{
+            $ret=$this->store->addUser('bob1','a@b.com','123456');
+            $ret=$this->store->addUser('bob2','a@b.com','123456');
+            self::fail('exception should have been thrown');
+
+        }catch (Exception $e){
+            $const=$this->logicalAnd(
+                $this->logicalNot($this->contains('bob2')),
+                $this->isType('array')
+            );
+            self::AssertThat($this->store->getUser('a@b.com'),$const);
+        }
     }
 }
